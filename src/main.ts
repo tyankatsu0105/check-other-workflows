3d59ce424e3d89dcbef49bcc731c0764f11a15c1
tyankatsu0105 <frips.ryilsufupe+dev@gmail.com>
2023-08-19 22:01:25 +0900

    check

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,42 @@ import * as github from "@actions/github";
 import { feature } from "./feature";
 import { getInput, Inputs } from "./input";
 
+const query = `query commitRef($owner: String!, $repo: String!, $expression: String!) {
+  repository(name: $repo, owner: $owner) {
+    object(expression: $expression) {
+      __typename
+      ... on Commit {
+        statusCheckRollup {
+          state
+          contexts(first: 100) {
+            totalCount
+            nodes {
+              __typename
+              ... on CheckRun {
+                completedAt
+                conclusion
+                permalink
+                startedAt
+                status
+                summary
+                text
+              }
+              ... on StatusContext {
+                createdAt
+                context
+                description
+                state
+                targetUrl
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`;
+
 const run = async () => {
   try {
     const inputs: Inputs = {
@@ -13,18 +49,16 @@ const run = async () => {
     const context = github.context;
     const octokit = github.getOctokit(inputs.token);
 
-    const { data } = await octokit.rest.checks.listForRef({
-      owner: context.repo.owner,
-      ref: context.ref,
-      repo: context.repo.repo,
-    });
+    const vars = { ...context.repo, expression: context.sha };
+
+    const res = await octokit.graphql(query, vars);
 
     // const hoge = await exec.exec(
     //   `gh pr checks ${context.payload.pull_request?.number}`
     // );
 
     core.debug(JSON.stringify(inputs, null, 2));
-    core.debug(JSON.stringify(data, null, 2));
+    core.debug(JSON.stringify(res, null, 2));
     // core.debug(`${hoge}`);
     // core.debug(JSON.stringify(context, null, 2));
 
