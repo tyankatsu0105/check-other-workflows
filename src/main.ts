7d9d4616bd60ded05a7e3184c50668bf90296710
tyankatsu0105 <frips.ryilsufupe+dev@gmail.com>
2023-08-19 12:06:42 +0900

    check

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,23 @@ import * as github from "@actions/github";
 import { feature } from "./feature";
 import { getInput, Inputs } from "./input";
 
-const run = () => {
+const run = async () => {
   try {
     const inputs: Inputs = {
       token: getInput("token"),
     };
     const context = github.context;
+    const octokit = github.getOctokit(inputs.token);
+
+    const { data } = await octokit.rest.pulls.get({
+      owner: context.repo.owner,
+      pull_number: context.payload.pull_request?.number ?? 0,
+      repo: context.repo.repo,
+    });
 
     core.debug(JSON.stringify(inputs, null, 2));
-    core.debug(JSON.stringify(context, null, 2));
+    core.debug(JSON.stringify(data, null, 2));
+    // core.debug(JSON.stringify(context, null, 2));
 
     // core.setOutput("time", new Date().toTimeString());
     feature({
